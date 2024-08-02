18e2746ac4bacc7dc69b3e59daf44c15a95c14b3
Takala <dylan91011028@gmail.com>
2024-08-02 14:40:08 +0800

    add: test_email.py

---
+++ src/test_email.py
@@ -0,0 +1,25 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(subject, body, to_email):
+    """Send email"""
+    from_email = os.getenv("EMAIL_USER")
+    from_password = os.getenv("EMAIL_PASSWORD")
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = from_email
+    msg["To"] = to_email
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(from_email, from_password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
+
+
+if __name__ == "__main__":
+    send_email(
+        "Book Notification Test", "This is a test email", os.getenv("EMAIL_USER")
+    )
