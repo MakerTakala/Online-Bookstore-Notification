f7c659deb008145377fd8a1847564c54ff6c9772
Takala <dylan91011028@gmail.com>
2024-08-02 21:59:44 +0800

    chore: Refactor main.py to use argparse for command line arguments and move email sending logic to BookSender class

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import os
-import smtplib
+import argparse
 from datetime import datetime
-from email.mime.text import MIMEText
-
 
 from base_type import Book
 from crawler import KingstoneCrawler
+from sender import BookSender
 
 
 def crawl_books(search_query: list[str]) -> list[Book]:
@@ -18,56 +17,29 @@ def crawl_books(search_query: list[str]) -> list[Book]:
     return book_list
 
 
-def filter_books(books: list[Book]) -> list[Book]:
+def filter_today_books(all_books: list[Book]) -> list[Book]:
     """Filter books"""
     allowed_books: list[Book] = []
-    for book in books:
+    for book in all_books:
         if datetime.today() == book.publish_date:
             allowed_books.append(book)
     return allowed_books
 
 
-def send_email(subject, body, to_email):
-    """Send email"""
-    from_email = os.getenv("EMAIL_USER")
-    from_password = os.getenv("EMAIL_PASSWORD")
-
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = from_email
-    msg["To"] = to_email
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(from_email, from_password)
-    server.sendmail(from_email, to_email, msg.as_string())
-    server.quit()
-
-
-def send_books(books: list[Book]):
-    """Send books to email"""
-    subject = "Today's new books"
-    context = "Today's new books are:\n"
-    book_show_html = ""
-    for book in books:
-        book_show_html += f"""
-        <div>
-            <h3>{book.title}</h3>
-            <img src="{book.image}" alt="{book.title}" />
-            <p>作者: {book.author}</p>
-            <p>價格: {book.price}</p>
-            <p>出版社: {book.publisher}</p>
-            <p>出版日期: {book.publish_date}</p>
-            <a href="{book.link}">連結</a>
-        </div>
-        """
-    part1 = MIMEText(context, "plain")
-    part2 = MIMEText(book_show_html, "html")
-    body = f"{part1.as_string()}\n{ part2.as_string()}"
-    send_email(subject, body, os.getenv("EMAIL_USER"))
-
-
 if __name__ == "__main__":
-    trace_books_name = os.getenv("QUERIES").split(",")
-    result_books = filter_books(crawl_books(trace_books_name))
-    if result_books:
-        send_books(result_books)
+    queries = os.getenv("QUERIES").split(",")
+    registers = os.getenv("REGISTER").split(",")
+    email_sender = os.getenv("EMAIL_SENDER")
+    email_sender_password = os.getenv("EMAIL_SENDER_PASSWORD")
+    book_sender = BookSender(email_sender, email_sender_password)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action="store_true")
+    args = parser.parse_args()
+
+    books = crawl_books(queries)
+    if args.test:
+        book_sender.send_email(registers, books)
+    else:
+        today_books = filter_today_books(books)
+        book_sender.send_email(registers, today_books)
---
+++ src/sender.py
@@ -0,0 +1,58 @@
+import smtplib
+from email.mime.text import MIMEText
+
+from base_type import Book
+
+
+class BookSender:
+    """Book sender class"""
+
+    def __init__(self, email_sender: str, email_sender_password: str):
+        self.email_sender = email_sender
+        self.email_sender_password = email_sender_password
+
+    def create_email_body(self, books: list[Book]):
+        """Send books to email"""
+        subject = "Today's new books"
+        book_show_html = "<h3>Today's new books</h3>"
+        for book in books:
+            book_show_html += f"""
+            <div>
+                <h3>{book.title}</h3>
+                <img src="{book.image}" alt="{book.title}" />
+                <p>作者: {book.author}</p>
+                <p>價格: {book.price}</p>
+                <p>出版社: {book.publisher}</p>
+                <p>出版日期: {book.publish_date}</p>
+                <a href="{book.link}">連結</a>
+            </div>
+            """
+
+        body = f"""
+        <html>
+            <head></head>
+            <body>
+                {book_show_html}
+            </body>
+        </html>
+        """
+        return subject, body
+
+    def send_email(self, rigisters: list[str], books: list[Book]):
+        """Send email"""
+
+        if not books:
+            return
+
+        subject, body = self.create_email_body(books)
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = self.email_sender
+
+        for register in rigisters:
+            msg["To"] = register
+
+            server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+            server.login(self.email_sender, self.email_sender_password)
+            server.sendmail(self.email_sender, register, msg.as_string())
+            server.quit()
